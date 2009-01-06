16bb298500a196afa5a291440f977593f297cb4a
Mitch.Waters.III <Mitch.Waters.III@baa961bc-d77b-11dd-8ca8-e3ff79f713b6>
2009-01-06 15:25:58 +0000

    revert jumble version to 1.0.0 from the trunk. Tests pass in eclipse, however there are classpath problems when running from maven. See the JavaRunner for the problem.

---
+++ jumble/jumble/src/test/java/com/reeltwo/jumble/dependency/DT5.java
@@ -0,0 +1,10 @@
+package com.reeltwo.jumble.dependency;
+
+/**
+ * Used in DependencyExtractorTest
+ * @author Tin Pavlinic
+ * @version $Revision: 496 $
+ */
+public class DT5 {
+  protected DT1 mDt1;
+}
